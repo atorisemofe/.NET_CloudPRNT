4f640a8d03448ddc7af6088ab163f44d08baf440
Oreoluwa Akeredolu <oakeredolu@starmicronics.com>
2026-04-06 11:40:49 -0400

    csproj quarantine remove for render

--- CloudPRNT_Solution/obj/Debug/net6.0/CloudPRNT_Solution.AssemblyInfo.cs
+++ CloudPRNT_Solution/obj/Debug/net6.0/CloudPRNT_Solution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudPRNT_Solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907d052333aa0688fce5c6c9b652aa36e57979f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ca83afa81bda2c1bad77328b3f69ab136f2550")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudPRNT_Solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudPRNT_Solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
